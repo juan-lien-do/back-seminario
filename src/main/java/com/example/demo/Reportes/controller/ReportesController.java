646c20c069d4d262122d3e6d8c66612d3055e2c2
juan-lien-do <findmejuan@hotmail.com>
2024-12-12 00:52:12 -0300

    agregar reporte completo

--- src/main/java/com/example/demo/Reportes/controller/ReportesController.java
+++ src/main/java/com/example/demo/Reportes/controller/ReportesController.java
@@ -4,12 +4,14 @@ import com.example.demo.Reportes.dto.ReporteCompletoDTO;
 import com.example.demo.Reportes.service.ReportesService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @RestController
@@ -21,7 +23,9 @@ public class ReportesController {
 
     // NO ANDA TODAVÍA
     @GetMapping("")
-    public ResponseEntity<ReporteCompletoDTO> conseguirReporteCompleto(@RequestParam Date fechaInicio, @RequestParam Date fechaFin){
+    public ResponseEntity<ReporteCompletoDTO> conseguirReporteCompleto(
+            @RequestParam(value = "fechaInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaInicio,
+            @RequestParam(value = "fechaFin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaFin){
         try {
             return ResponseEntity.ok(reportesService.conseguirReporteCompleto(fechaInicio, fechaFin));
         }catch (Exception e){
--- src/main/java/com/example/demo/Reportes/service/ReportesService.java
+++ src/main/java/com/example/demo/Reportes/service/ReportesService.java
@@ -6,13 +6,15 @@ import com.example.demo.Recursos.domain.RegistroIncorporacion;
 import com.example.demo.Recursos.repository.RegistroIncorporacionRepository;
 import com.example.demo.Reportes.dto.ReporteCompletoDTO;
 import lombok.AllArgsConstructor;
+import org.antlr.v4.runtime.misc.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -22,9 +24,7 @@ public class ReportesService {
     @Autowired
     private RegistroIncorporacionRepository registroIncorporacionRepository;
 
-    public ReporteCompletoDTO conseguirReporteCompleto(Date fi, Date ff) throws Exception{
-        LocalDate fechaInicio = fi.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        LocalDate fechaFin = ff.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    public ReporteCompletoDTO conseguirReporteCompleto(LocalDate fechaInicio, LocalDate fechaFin) throws Exception {
 
         ReporteCompletoDTO.ReporteCompletoDTOBuilder builder = ReporteCompletoDTO.builder();
         builder.fechaFin(fechaFin);
@@ -40,14 +40,14 @@ public class ReportesService {
         Long pedidosCompletados = envios.stream().filter(x -> {
                     Long ultimoEstado = x.getListaCambiosEstado()
                             .stream()
-                            .filter( ce -> ce.getFechaFin() == null)
+                            .filter(ce -> ce.getFechaFin() == null)
                             .findFirst()
                             .get()
                             .getIdEstadoEnvio();
 
                     return ultimoEstado == 4L || ultimoEstado == 5L || ultimoEstado == 6L;
                 })
-                        .count();
+                .count();
 
 
         // ni entregados, ni devueltos, ni cancelados
@@ -55,7 +55,7 @@ public class ReportesService {
                 x -> {
                     Long ultimoEstado = x.getListaCambiosEstado()
                             .stream()
-                            .filter( ce -> ce.getFechaFin() == null)
+                            .filter(ce -> ce.getFechaFin() == null)
                             .findFirst()
                             .get()
                             .getIdEstadoEnvio();
@@ -76,7 +76,7 @@ public class ReportesService {
                 x -> {
                     Long ultimoEstado = x.getListaCambiosEstado()
                             .stream()
-                            .filter( ce -> ce.getFechaFin() == null)
+                            .filter(ce -> ce.getFechaFin() == null)
                             .findFirst()
                             .get()
                             .getIdEstadoEnvio();
@@ -85,11 +85,109 @@ public class ReportesService {
                 }).toList();
 
 
+        // buscar los meses entre las dos fechas
+        DateTimeFormatter monthFormatter = DateTimeFormatter.ofPattern("MMM/uuuu", Locale.ROOT);
+        List<YearMonth> meses = new ArrayList<>();
+        for (YearMonth month = YearMonth.from(fechaInicio);
+             !month.isAfter(YearMonth.from(fechaFin));
+             month = month.plusMonths(1)) {
+            meses.add(month);
+        }
 
+        //tiempos promedio de procesamiento
+        List<Float> tiemposPromedioProcesamiento = new ArrayList<>();
 
+        for (YearMonth mes : meses) {
+            float acumulado = 0;
+            long cantidad = 0L;
 
+            for (Envio env : enviosSinCancelar) {
+                cantidad += 1L;
+                acumulado += env.calcularTiempoDeProcesamiento();
+            }
 
+            tiemposPromedioProcesamiento.add(cantidad / (float) acumulado);
+            //continuar
+        }
+
+        builder.meses(meses.stream().map(mes -> mes.format(monthFormatter)).toList());
+        builder.tiemposPromedioProcesamiento(tiemposPromedioProcesamiento);
+
+        // estados
+        // pendiente 1, en preparacion 2, en reparacion 9, para retiro 8
+        List<String> estados = new ArrayList<>(); //List.of("Pendiente", "En preparación", "En reparación", "Para retiro");
+        List<Float> tiempoPorEstado = new ArrayList<>();
+        HashMap<String, Long> mapaEstados = new HashMap<>();
+
+        mapaEstados.put("Pendiente", 1L);
+        mapaEstados.put("En preparación", 2L);
+        mapaEstados.put("En reparación", 9L);
+        mapaEstados.put("Para retiro", 8L);
+
+        for (Map.Entry<String, Long> entry: mapaEstados.entrySet()) {
+            estados.add(entry.getKey());
+            Float tiempo = 0f;
+            Long cantidad = 0L;
+            for (Envio env : enviosSinCancelar){
+                Float tEnvio = env.calcularTiempoPorEstado(entry.getValue());
+                tiempo += tEnvio;
+                cantidad += tEnvio == 0f ? 0L : 1L;
+            }
+
+            tiempoPorEstado.add(tiempo / (float) cantidad);
+        }
+
+        builder.estados(estados);
+        builder.tiemposPromedioEstado(tiempoPorEstado);
+
+        List<String> elementos = new ArrayList<>();
+        List<Long> cantidadElem = new ArrayList<>();
+
+        // contar computadoras
+        Long cantPc = 0L;
+        for (Envio env : enviosSinCancelar){
+            cantPc += env.contarPc();
+        }
+        elementos.add("PC");
+        cantidadElem.add(cantPc);
+
+        Long cantNotebooks = 0L;
+        for (Envio env: enviosSinCancelar){
+            cantNotebooks += env.contarNotebooks();
+        }
+        elementos.add("Notebooks");
+        cantidadElem.add(cantNotebooks);
+
+        Long cantAIO = 0L;
+        for (Envio env : enviosSinCancelar){
+            cantAIO += env.contarAllInOne();
+        }
+        elementos.add("All In One");
+        cantidadElem.add(cantAIO);
+
+        // TODO contar recursos
+        Long cantPerif = 0L;
+        for (Envio env : enviosSinCancelar){
+            cantPerif += env.contarPerifericos();
+        }
+        elementos.add("Periféricos");
+        cantidadElem.add(cantPerif);
+
+        Long cantComponentes = 0L;
+        for (Envio env : enviosSinCancelar){
+            cantComponentes += env.contarComponentes();
+        }
+        elementos.add("Componentes");
+        cantidadElem.add(cantComponentes);
+
+        builder.elementos(elementos);
+        builder.cantidades(cantidadElem);
 
         return builder.build();
     }
+
+
+
+
+
 }
